3c6e464a640eeb13411b130a53c8f2a979d46e8a
deboer.md <deboer.md@ed39468c-8a2b-0410-9ce3-9127c112da76>
2007-12-29 16:46:00 +0000

    * added settings for each plugin (through an XML specification) * implemented/ improved the toolbox

--- var/plugins/toolbox/tools/watermark.tool.php
+++ var/plugins/toolbox/tools/watermark.tool.php
@@ -12,6 +12,55 @@
 defined('_ZMG_EXEC') or die('Restricted access');
 
 class zmgWatermarkTool {
+    function apply($src_file, $dest_file, $wm_file, $position, &$imgobj) {
+        $imginfo_wm = getimagesize($wm_file);
 
+        $imagewidth = $imgobj->_size[0];
+        $imageheight = $imgobj->_size[1];
+        $watermarkwidth = $imginfo_wm[0];
+        $watermarkheight = $imginfo_wm[1];      
+        $width_left = $imagewidth - $watermarkwidth;
+        $height_left = $imageheight - $watermarkheight;
+        switch ($position) {
+            case "TL": // Top Left
+                $startwidth = $width_left >= 5 ? 4 : $width_left;
+                $startheight = $height_left >= 5 ? 5 : $height_left;
+                break;
+            case "TM": // Top middle 
+                $startwidth = intval(($imagewidth - $watermarkwidth) / 2);
+                $startheight = $height_left >= 5 ? 5 : $height_left;
+                break;
+            case "TR": // Top right
+                $startwidth = $imagewidth - $watermarkwidth-4;
+                $startheight = $height_left >= 5 ? 5 : $height_left;
+                break;
+            case "CL": // Center left
+                $startwidth = $width_left >= 5 ? 4 : $width_left;
+                $startheight = intval(($imageheight - $watermarkheight) / 2);
+                break;
+            default:
+            case "C": // Center (the default)
+                $startwidth = intval(($imagewidth - $watermarkwidth) / 2);
+                $startheight = intval(($imageheight - $watermarkheight) / 2);
+                break;
+            case "CR": // Center right
+                $startwidth = $imagewidth - $watermarkwidth-4;
+                $startheight = intval(($imageheight - $watermarkheight) / 2);
+                break;
+            case "BL": // Bottom left
+                $startwidth = $width_left >= 5 ? 5 : $width_left;
+                $startheight = $imageheight - $watermarkheight-5;
+                break;
+            case "BM": // Bottom middle
+                $startwidth = intval(($imagewidth - $watermarkwidth) / 2);
+                $startheight = $imageheight - $watermarkheight-5;
+                break;
+            case "BR": // Bottom right
+                $startwidth = $imagewidth - $watermarkwidth-4;
+                $startheight = $imageheight - $watermarkheight-5;
+                break;
+        }
+        //TODO: implement call to image library too watermark() function
+    }
 }
 ?>
